c50961acdde8f67711cb0e26a8afdd82ec846a6d
Petter Rodhelind <petter.rodhelind@gmail.com>
2017-03-10 18:10:56 +0100

    add command line arguments

--- main.go
+++ main.go
@@ -5,15 +5,28 @@ https://tools.ietf.org/html/rfc1436
 */
 package main
 
-import "net"
+import (
+	"flag"
+	"net"
+	"os"
+	"strconv"
+)
 
-const (
-	host string = "localhost"
-	port string = "7070"
-	root string = "."
+var (
+	help = flag.Bool("h", false, "Show usage")
+	host = flag.String("a", "localhost", "Listening host address")
+	port = flag.Int("p", 70, "Listening port")
+	root = flag.String("d", "/var/gopher", "Root directory to serve")
 )
 
 func main() {
-	addr := net.JoinHostPort(host, port)
+	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	addr := net.JoinHostPort(*host, strconv.Itoa(*port))
 	ListenAndServe(addr)
 }
